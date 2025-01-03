9a3ee08f6eca0b349a5bd40e83347729eb870ca2
Nathan <nathanplouvier60@gmail.com>
2025-01-03 22:41:43 +0100

    Update domain for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: ["xtxqjsmgkwqwfyqyfwdm.supabase.co"],
     remotePatterns: [
       {
         protocol: "https",
