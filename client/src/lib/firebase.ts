4ee835daf3edde9f9fa52e268779d88c0f0bda34
Nathan <nathanplouvier60@gmail.com>
2025-03-29 01:52:26 +0100

    Remove hardcoded api keys from client

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -4,29 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Your Firebase config from Firebase Console
-// const firebaseConfig = {
-// 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-// 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-// 	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-// 	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-// 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-// 	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-	apiKey: 'AIzaSyDFpW552jNRgfTujizaMI2beyFzKYE8rFs',
-
-	authDomain: 'project-orion-prod.firebaseapp.com',
-
-	projectId: 'project-orion-prod',
-
-	storageBucket: 'project-orion-prod.firebasestorage.app',
-
-	messagingSenderId: '528381855325',
-
-	appId: '1:528381855325:web:37ccf638ad4dabca4e6fb4',
-
-	measurementId: 'G-3K1CDZ6YLS',
+	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase app if not already initialized
