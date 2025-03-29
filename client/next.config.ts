94e8a8df0534f5241c9575ece7d02084019e1ae0
Nathan <nathanplouvier60@gmail.com>
2025-03-29 01:46:44 +0100

    Test new nginx configuration

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+	rewrites: async () => [
+		{
+			source: '/api/:path*',
+			destination: 'http://backend:8080/:path*',
+		},
+	],
 	images: {
 		unoptimized: true,
 		domains: [],
